f8bf169260b8cc252afbbce5155764f32d95cd7d
robbieg9876 <robbiegiles@hotmail.co.uk>
2020-10-13 15:34:11 +0100

    Committed

---
+++ source/repos/Assessment1/Assessment1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Assessment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtCommandLine = new System.Windows.Forms.TextBox();
+            this.lblnput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblCommandLine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.Menu;
+            this.txtOutput.Location = new System.Drawing.Point(936, 32);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(871, 548);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Visible = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.ForeColor = System.Drawing.SystemColors.Menu;
+            this.txtInput.Location = new System.Drawing.Point(12, 32);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(871, 548);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtCommandLine
+            // 
+            this.txtCommandLine.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtCommandLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCommandLine.ForeColor = System.Drawing.SystemColors.Menu;
+            this.txtCommandLine.Location = new System.Drawing.Point(12, 654);
+            this.txtCommandLine.Multiline = true;
+            this.txtCommandLine.Name = "txtCommandLine";
+            this.txtCommandLine.Size = new System.Drawing.Size(1795, 262);
+            this.txtCommandLine.TabIndex = 2;
+            this.txtCommandLine.TextChanged += new System.EventHandler(this.txtCommandLine_TextChanged);
+            // 
+            // lblnput
+            // 
+            this.lblnput.AutoSize = true;
+            this.lblnput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnput.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblnput.Location = new System.Drawing.Point(7, 4);
+            this.lblnput.Name = "lblnput";
+            this.lblnput.Size = new System.Drawing.Size(150, 25);
+            this.lblnput.TabIndex = 3;
+            this.lblnput.Text = "Program Code";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblOutput.Location = new System.Drawing.Point(942, 4);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(91, 25);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Console";
+            this.lblOutput.Visible = false;
+            // 
+            // lblCommandLine
+            // 
+            this.lblCommandLine.AutoSize = true;
+            this.lblCommandLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCommandLine.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCommandLine.Location = new System.Drawing.Point(12, 612);
+            this.lblCommandLine.Name = "lblCommandLine";
+            this.lblCommandLine.Size = new System.Drawing.Size(156, 25);
+            this.lblCommandLine.TabIndex = 5;
+            this.lblCommandLine.Text = "Command Line";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1827, 928);
+            this.Controls.Add(this.lblCommandLine);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblnput);
+            this.Controls.Add(this.txtCommandLine);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtCommandLine;
+        private System.Windows.Forms.Label lblnput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblCommandLine;
+    }
+}
+
---
+++ source/repos/Assessment1/Assessment1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+       
+        private void txtCommandLine_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtCommandLine_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Console.WriteLine("ENTER PRESSED");
+            }
+        }
+    }
+}
