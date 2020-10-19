c2935dc493d54c73b8c282c7257a0a5f32959227
robbieg9876 <robbiegiles@hotmail.co.uk>
2020-10-19 15:45:56 +0100

    Committed

--- source/repos/Assessment1/Assessment1/Canvass.cs
+++ source/repos/Assessment1/Assessment1/Canvass.cs
@@ -5,13 +5,15 @@ using System.Linq;
 namespace Assessment1
 {
     public class Canvass
-    {
+    { 
+        //Initialises graphics variables
         Graphics g;
         Pen Pen;
         int xPos, yPos;
 
         public Canvass(Graphics g)
         {
+            //Sets initial values for graphics
             this.g = g;
             xPos = yPos = 0;
             Pen = new Pen(Color.White, 1);
@@ -23,15 +25,19 @@ namespace Assessment1
 
         public void MoveTo(int toX, int toY)
         {
+            //checks if inputs are valid
             if (toX < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", toX, "Point is out of range");
             }
            
             if (toY < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", toY, "Point is out of range");
             }
+            //Moves the drawing start point to the values inputted
             xPos = toX;
             yPos = toY;
               
@@ -39,86 +45,112 @@ namespace Assessment1
 
         public void DrawTo(int toX, int toY)
         {
+            //checks if inputs are valid
             if (toX < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", toX, "Point is out of range");
             }
 
             if (toY < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", toY, "Point is out of range");
             }
+            // Draws a line from the current position to the inputted co-ordinates
             g.DrawLine(Pen, xPos, yPos, toX, toY);
+            //Changes current drawing position to where the line finishes
             xPos = toX;
             yPos = toY;
         }
 
         public void DrawSquare(int width)
         {
+            //Checks that the shape will fit on the graphics panel
             if (xPos+width <0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", xPos+width, "Point is out of range");
             }
 
             if (xPos +width < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", yPos+width, "Point is out of range");
             }
+            //Draws a square from the current drawing postion with sides of the width inputted
             g.DrawRectangle(Pen, xPos, yPos, xPos + width, yPos + width);
         }
         public void DrawCircle(int radius)
         {
+            //Checks that the shape will fit on the graphics panel
             if (xPos - radius < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", xPos - radius , "Point is out of range");
             }
 
             if (xPos + radius < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", xPos + radius, "Point is out of range");
             }
 
             if (yPos - radius < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", yPos - radius, "Point is out of range");
             }
 
             if (yPos + radius < 0)
             {
+                //Throws exception if input is invalid
                 throw new System.ArgumentOutOfRangeException("MoveTo", yPos + radius, "Point is out of range");
             }
-
+            //Draws a circle with the radius inputted
             g.DrawEllipse(Pen, xPos - radius, yPos - radius, xPos + radius, yPos + radius);
         }
         public void DrawTriangle(int width, int height)
         {
+            //Creates an array with 3 points
+            //User inputs the width and height
+            // 3 Points are starting point, horizantally from start point, vertically from start point
             Point[] Triangle = new Point[] { new Point(xPos, yPos), new Point(xPos + width, yPos), new Point(xPos, yPos + height) };
+            //Draws lines bewtween the points to make a triangle
             g.DrawPolygon(Pen, Triangle);
         }
 
         public void DrawRectangle(int width, int height)
         {
+            //Draws a rectangle from the start drawing point with width and height values inputted
             g.DrawRectangle(Pen, xPos, yPos, xPos + width, yPos + height);
         }
         public void PenColour(Color colour)
         {
+            //Changes the pen colour to the one inputted
             Pen.Color = colour;
         }
 
         public void FillSquare(int width)
         {
+            //Draws a sqaure from the start drawing point with width values inputted
+            //Fills the square with the current pen colour
             SolidBrush solidBrush = new SolidBrush(Pen.Color);
             g.FillRectangle(solidBrush, xPos, yPos, xPos + width, yPos + width);
         }
 
         public void FillCircle(int radius)
         {
+            //Draws a circle with the radius inputted
+            //Fills the circle with the current pen colour
             SolidBrush solidBrush = new SolidBrush(Pen.Color);
             g.FillEllipse(solidBrush, xPos - radius, yPos - radius, xPos + radius, yPos + radius);
         }
 
         public void FillTriangle(int width, int height)
         {
+            //Draws lines bewtween the points to make a triangle
+            //Fills the triangle with the current pen colour
             Point[] Triangle = new Point[] { new Point(xPos, yPos), new Point(xPos + width, yPos), new Point(xPos, yPos + height) };
             SolidBrush solidBrush = new SolidBrush(Pen.Color);
             g.FillPolygon(solidBrush, Triangle);
@@ -126,16 +158,20 @@ namespace Assessment1
 
         public void FillRectangle(int width, int height)
         {
+            //Draws a rectangle from the start drawing point with width and height values inputted
+            //Fills the rectangle with the current pen colour
             SolidBrush solidBrush = new SolidBrush(Pen.Color);
             g.FillRectangle(solidBrush, xPos, yPos, xPos + width, yPos + height);
         }
 
         public void clearArea(Color colour)
         {
+            //Clears drawing surface and sets background colour
             g.Clear(colour);
         }
         public void resetPenPosition()
         {
+            //Resets the start drawing position to (0,0) (top left)
             xPos = yPos = 0;
         }
     }
--- source/repos/Assessment1/Assessment1/Form1.cs
+++ source/repos/Assessment1/Assessment1/Form1.cs
@@ -15,12 +15,16 @@ namespace Assessment1
 {
     public partial class Form1 : Form
     {
+        //Initialises bitmap with set size
         Bitmap OutputBitMap = new Bitmap(640, 480);
+        //Makes an instance of the Canvass class
         Canvass MyCanvass;
+        //Initialises variables
         Boolean Fill = false;
         String[] CommandSplit;
         public Form1()
         {
+            //Sets graphics up on bitmap
             InitializeComponent();
             MyCanvass = new Canvass(Graphics.FromImage(OutputBitMap));
         }
@@ -34,20 +38,29 @@ namespace Assessment1
         }
         private void txtCommandLine_KeyDown(object sender, KeyEventArgs e)
         {
-
+            //Checks if enter key is pressed
             if (e.KeyCode == Keys.Enter)
             {
+                //Stores the input in a string
+                //Removes whitespace and converts letters to lower case
                 String Command = txtCommandLine.Text.Trim().ToLower();
+                //Splits the string and stores values in commandSplit Array
                 CommandSplit = Command.Split(' ');
 
+                //Checks if the first value in the array is a valid command
                 if (CommandSplit[0].Equals("run") == true)
                 {
+                    //This takes all the text in the input box
+                    //Splits it into individual lines
                     using (StringReader reader = new StringReader(txtInput.Text))
                     {
+                        //Loops through each line in the input box
                         string line;
                         while ((line = reader.ReadLine()) != null)
                         {
+                            //Splits line and stores values in commandSplit Array
                             CommandSplit = line.Split(' ');
+                            //Calls command method
                             command();
                         }
                     }
@@ -55,18 +68,23 @@ namespace Assessment1
                 }
                 else if (CommandSplit[0].Equals("save") == true)
                 {
+                    //splits string and stores values in commandSplit Array
                     CommandSplit = Command.Split(' ');
+                    //Sets folder for file to be saved into
                     string path = @"C:\Users\robbi\source\repos\Assessment1\Assessment1\";
-
+                    //Checks if the folder already exists
                     if (Directory.Exists(path))
                     {
+                        //Adds file with the name inputted to the folder
                         File.AppendAllText(path + CommandSplit[1], txtInput.Text);
                     }
 
                     else
                     {
+                        //Creates the folder set above
                         Directory.CreateDirectory(path);
-                        File.AppendAllText(path + CommandSplit[1], "The first line");
+                        // Adds file with the name inputted to the folder
+                        File.AppendAllText(path + CommandSplit[1], txtInput.Text);
                     }
 
 
@@ -75,16 +93,24 @@ namespace Assessment1
                 }
                 else if (CommandSplit[0].Equals("load") == true)
                 {
+                    //Sets target file to load in
                     string path = @"C:\Users\robbi\source\repos\Assessment1\Assessment1\";
+                    //Gets all the text from the file
                     string text = System.IO.File.ReadAllText(path + CommandSplit[1]);
+                    //Clears input box
                     txtInput.Clear();
+                    //Fills input box with text from the file
                     txtInput.AppendText(text);
                 }
                 else
+                //This means that they aren't using the input box so will only look for commands from  the command line
                 {
+                    //Splits strings and stores values in CommandSplit array
                     CommandSplit = Command.Split(' ');
+                    //Calls command method
                     command();
                 }
+                //Clears command line
                 txtCommandLine.Text = "";
                 Refresh();
             }
@@ -92,81 +118,112 @@ namespace Assessment1
 
         private void command()
         {
+            //Checks for valid commands
             if (CommandSplit[0].Equals("position") == true)
             {
                 if (CommandSplit[1].Equals("pen") == true)
                 {
+                    //Recieves two inputs and stores as integers
                     int x = int.Parse(CommandSplit[2]);
                     int y = int.Parse(CommandSplit[3]);
+                    //Passes the values as parameters to MyCanvass.MoveTo 
                     MyCanvass.MoveTo(x, y);
+                    //Writes message to console
                     Console.WriteLine("POSITION PEN");
                 }
             }
 
             else if (CommandSplit[0].Equals("square") == true)
             {
+                //Recieves input and strores as an integer
                 int length = int.Parse(CommandSplit[1]);
+                //Passes the value as a parameter to MyCanvass.DrawSquare
                 MyCanvass.DrawSquare(length);
+                //Checks if fill is on
                 if (Fill)
                 {
+                    //Fills shape
                     MyCanvass.FillSquare(length);
                 }
+                //Writes to console
                 Console.WriteLine("SQUARE");
             }
             else if (CommandSplit[0].Equals("circle") == true)
             {
+                //Recieves input and strores as an integer
                 int radius = int.Parse(CommandSplit[1]);
+                //Passes the value as a parameter to MyCanvass.DrawCircle
                 MyCanvass.DrawCircle(radius);
+                //Checks if fill is on
                 if (Fill)
                 {
+                    //Fills shape
                     MyCanvass.FillCircle(radius);
                 }
+                //Writes to console
                 Console.WriteLine("CIRCLE");
             }
             else if (CommandSplit[0].Equals("triangle") == true)
             {
+                //Recieves two inputs and stores as integers
                 int width = int.Parse(CommandSplit[1]);
                 int height = int.Parse(CommandSplit[2]);
+                //Passes the values as parameters to MyCanvass.DrawTriangle
                 MyCanvass.DrawTriangle(width, height);
+                //Checks if fill is on
                 if (Fill)
                 {
-
+                    //Fills shape
                     MyCanvass.FillTriangle(width, height);
                 }
+                //Writes to console
                 Console.WriteLine("TRIANGLE");
             }
             else if (CommandSplit[0].Equals("rectangle") == true)
             {
+                //Recieves two inputs and stores as integers
                 int width = int.Parse(CommandSplit[1]);
                 int height = int.Parse(CommandSplit[2]);
+                //Passes the values as parameters to MyCanvass.DrawRectangle
                 MyCanvass.DrawRectangle(width, height);
+                //Checks if fill is on
                 if (Fill)
                 {
+                    //Fills shape
                     MyCanvass.FillRectangle(width, height);
                 }
+                //Writes to console
                 Console.WriteLine("SQUARE");
             }
             else if (CommandSplit[0].Equals("clear") == true)
             {
+                //Passes the colour of the output window as a parameter to MyCanvass.clearArea
                 MyCanvass.clearArea(OutputWindow.BackColor);
+                //Writes to console
                 Console.WriteLine("CLEAR");
             }
             else if (CommandSplit[0].Equals("reset") == true)
             {
+                //Calls MyCanvass,resetPenPosition to put starting drawing position back to (0,0)
                 MyCanvass.resetPenPosition();
+                //Writes to console
                 Console.WriteLine("RESET PEN");
             }
             else if (CommandSplit[0].Equals("fill") == true)
             {
                 if (CommandSplit[1].Equals("on") == true)
                 {
+                    //Turns fill on by making boolean Fill true
                     Fill = true;
+                    //Writes to console
                     Console.WriteLine("FILL ON");
                 }
 
                 else if (CommandSplit[1].Equals("off") == true)
                 {
+                    //Turns fill off by making boolean Fill false
                     Fill = false;
+                    //Writes to console
                     Console.WriteLine("FILL OFF");
                 }
             }
@@ -174,34 +231,47 @@ namespace Assessment1
             {
                 if (CommandSplit[1].Equals("red") == true)
                 {
+                    //Passes the colour inputted as a parameter to MyCanvass.PenColour
                     MyCanvass.PenColour(Color.Red);
+                    //Writes to console
                     Console.WriteLine("PEN RED");
                 }
                 else if (CommandSplit[1].Equals("green") == true)
                 {
+                    //Passes the colour inputted as a parameter to MyCanvass.PenColour
                     MyCanvass.PenColour(Color.Green);
+                    //Writes to console
                     Console.WriteLine("PEN GREEN");
                 }
                 else if (CommandSplit[1].Equals("blue") == true)
                 {
+                    //Passes the colour inputted as a parameter to MyCanvass.PenColour
                     MyCanvass.PenColour(Color.Blue);
+                    //Writes to console
                     Console.WriteLine("PEN BLUE");
                 }
                 else if (CommandSplit[1].Equals("yellow") == true)
                 {
+                    //Passes the colour inputted as a parameter to MyCanvass.PenColour
                     MyCanvass.PenColour(Color.Yellow);
+                    //Writes to console
                     Console.WriteLine("PEN YELLOW");
                 }
                 else if (CommandSplit[1].Equals("white") == true)
                 {
+                    //Passes the colour inputted as a parameter to MyCanvass.PenColour
                     MyCanvass.PenColour(Color.White);
+                    //Writes to console
                     Console.WriteLine("PEN WHITE");
                 }
                 else if (CommandSplit[1].Equals("draw") == true)
                 {
+                    //Recieves two inputs and stores as integers
                     int x = int.Parse(CommandSplit[2]);
                     int y = int.Parse(CommandSplit[3]);
+                    //Passes the values as parameters to MyCanvass.DrawTo
                     MyCanvass.DrawTo(x, y);
+                    //Writes to console
                     Console.WriteLine("PEN DRAW");
                 }
             }
@@ -209,6 +279,7 @@ namespace Assessment1
 
         private void OutputWindow_Paint(object sender, PaintEventArgs e)
         {
+            //Initialises the graphics on the output window
             Graphics g = e.Graphics;
             g.DrawImageUnscaled(OutputBitMap, 0, 0);
         }
