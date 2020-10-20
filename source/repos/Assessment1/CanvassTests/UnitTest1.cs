6395500953b0b01383c5483175a57591588ea273
robbieg9876 <robbiegiles@hotmail.co.uk>
2020-10-20 18:57:46 +0100

    More tests added

--- source/repos/Assessment1/CanvassTests/UnitTest1.cs
+++ source/repos/Assessment1/CanvassTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using Assessment1;
+using System;
+
 namespace CanvassTests
 {
     
@@ -8,9 +10,9 @@ namespace CanvassTests
     public class UnitTest1 {
         //Initialises bitmap with set size
         Bitmap OutputBitMap = new Bitmap(871, 548);
+
         //Makes an instance of the Canvass class
-        Canvass Test;
-        Form1 test;
+        readonly Canvass Test;
         public UnitTest1()
         {
             //Sets graphics up on bitmap
@@ -47,9 +49,9 @@ namespace CanvassTests
         public void PenColourCheck()
         {
             //Sets integer variables
-           Color colour = Color.White;
+           Color colour = Color.Red;
             //Passes values as parameters to Test.MoveTo
-            //Test.PenColour(colour);
+            Test.PenColour(colour);
             //Checks if the statements are true as expected
             Assert.AreEqual(colour, Test.Pen.Color);
             
@@ -66,13 +68,99 @@ namespace CanvassTests
             Assert.IsTrue(toX == Test.xPos);
             Assert.IsTrue(toY == Test.yPos);
         }
-        public void ClearTest()
+
+        [TestMethod]
+        public void FillOnTest()
         {
-            
-            //Passes values as parameters to Test.MoveTo
-            Test.clearArea(Color.Black);
+            //Sets integer variables
+            Boolean fill = true;
             //Checks if the statements are true as expected
-            
+            Test.FillShape(fill);
+
+            Assert.AreEqual(fill,Test.Fill);
+        }
+        [TestMethod]
+        public void FillOffTest()
+        {
+            //Sets integer variables
+            Boolean fill = false;
+            //Checks if the statements are true as expected
+            Test.FillShape(fill);
+
+            Assert.AreEqual(fill, Test.Fill);
+        }
+
+        [TestMethod]
+        public void DrawSquareTest()
+        {
+            //Sets integer variables
+            int width = 50;
+            int toX = Test.xPos;
+            int toY = Test.yPos;
+            //Passes values as parameters to Test.MoveTo
+            Test.DrawSquare(width);
+            //Checks if the statements are true as expected
+            //Checks drawing position has not changed
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+            //Checks shape is the same size as expected
+            Assert.IsTrue(toX + width == Test.xPos + width);
+            Assert.IsTrue(toY + width == Test.yPos + width);
+        }
+        [TestMethod]
+        public void DrawCircleTest()
+        {
+            //Sets integer variables
+            int radius = 50;
+            int toX = Test.xPos;
+            int toY = Test.yPos;
+            //Passes values as parameters to Test.MoveTo
+            Test.DrawCircle(radius);
+            //Checks if the statements are true as expected
+            //Checks drawing position has not changed
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+            //Checks shape is the same size as expected
+            Assert.IsTrue(toX + radius == Test.xPos + radius);
+            Assert.IsTrue(toY + radius == Test.yPos + radius);
+            Assert.IsTrue(toX - radius == Test.xPos - radius);
+            Assert.IsTrue(toY - radius == Test.yPos - radius);
+        }
+        [TestMethod]
+        public void DrawTriangleTest()
+        {
+            //Sets integer variables
+            int width = 50;
+            int height = 150;
+            int toX = Test.xPos;
+            int toY = Test.yPos;
+            //Passes values as parameters to Test.MoveTo
+            Test.DrawTriangle(width,height);
+            //Checks if the statements are true as expected
+            //Checks drawing position has not changed
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+            //Checks shape is the same size as expected
+            Assert.IsTrue(toX + width == Test.xPos + width);
+            Assert.IsTrue(toY + height == Test.yPos + height);
+        }
+        [TestMethod]
+        public void DrawRectangleTest()
+        {
+            //Sets integer variables
+            int width = 50;
+            int height = 150;
+            int toX = Test.xPos;
+            int toY = Test.yPos;
+            //Passes values as parameters to Test.MoveTo
+            Test.DrawRectangle(width, height);
+            //Checks if the statements are true as expected
+            //Checks drawing position has not changed
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+            //Checks shape is the same size as expected
+            Assert.IsTrue(toX + width == Test.xPos + width);
+            Assert.IsTrue(toY + height == Test.yPos + height);
         }
     }
 }
