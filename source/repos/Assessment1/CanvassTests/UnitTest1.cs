41b643a3e75f7560d0946d46dc1939d330ea4989
robbieg9876 <robbiegiles@hotmail.co.uk>
2020-10-20 15:37:16 +0100

    More tests added

--- source/repos/Assessment1/CanvassTests/UnitTest1.cs
+++ source/repos/Assessment1/CanvassTests/UnitTest1.cs
@@ -6,17 +6,19 @@ namespace CanvassTests
     
     [TestClass]
     public class UnitTest1 {
-        //Initialises variables
-        Graphics g;
-        //Creates an instance of Canvass.
-        Canvass Test = new Canvass();
-      
-        [TestMethod]
-        public void TestMethod1()
+        //Initialises bitmap with set size
+        Bitmap OutputBitMap = new Bitmap(871, 548);
+        //Makes an instance of the Canvass class
+        Canvass Test;
+        Form1 test;
+        public UnitTest1()
         {
+            //Sets graphics up on bitmap
+            Test = new Canvass(Graphics.FromImage(OutputBitMap));
         }
+
         [TestMethod]
-        public void MoveToWithValidInputs__MovesPen()
+        public void MoveToTest()
         {
             //Sets integer variables
             int toX = 250;
@@ -24,18 +26,53 @@ namespace CanvassTests
             //Passes values as parameters to Test.MoveTo
             Test.MoveTo(toX, toY);
             //Checks if the statements are true as expected
-            Assert.IsTrue(toX > 0);
-            Assert.IsTrue(toY > 0);
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY ==Test.yPos);
         }
 
         [TestMethod]
-        public void MoveToWithInvalidInputs__ShouldThrowArgumentOutOfRange()
+        public void DrawToTest()
         {
             //Sets integer variables
-            int toX = -250;
-            int toY = -250;
-            //Checks that an exception is thrown as expected
-            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => Test.MoveTo(toX, toY));
+            int toX = 250;
+            int toY = 250;
+            //Passes values as parameters to Test.MoveTo
+            Test.DrawTo(toX, toY);
+            //Checks if the statements are true as expected
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+        }
+
+        [TestMethod]
+        public void PenColourCheck()
+        {
+            //Sets integer variables
+           Color colour = Color.White;
+            //Passes values as parameters to Test.MoveTo
+            //Test.PenColour(colour);
+            //Checks if the statements are true as expected
+            Assert.AreEqual(colour, Test.Pen.Color);
+            
+        }
+        [TestMethod]
+        public void ResetPenTest()
+        {
+            //Sets integer variables
+            int toX = 0;
+            int toY = 0;
+            //Passes values as parameters to Test.MoveTo
+            Test.resetPenPosition();
+            //Checks if the statements are true as expected
+            Assert.IsTrue(toX == Test.xPos);
+            Assert.IsTrue(toY == Test.yPos);
+        }
+        public void ClearTest()
+        {
+            
+            //Passes values as parameters to Test.MoveTo
+            Test.clearArea(Color.Black);
+            //Checks if the statements are true as expected
+            
         }
     }
 }
