cd794fc60c41605030336ef34b2107a41cda3083
robbieg9876 <robbiegiles@hotmail.co.uk>
2020-10-22 17:57:01 +0100

    Added tests for componenet 2

--- source/repos/Assessment1/CanvassTests/UnitTest1.cs
+++ source/repos/Assessment1/CanvassTests/UnitTest1.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using Assessment1;
 using System;
+using System.Linq;
+using Microsoft.VisualBasic;
 
 namespace CanvassTests
 {
@@ -161,6 +163,59 @@ namespace CanvassTests
             //Checks shape is the same size as expected
             Assert.IsTrue(toX + width == Test.xPos + width);
             Assert.IsTrue(toY + height == Test.yPos + height);
+        }
+        [TestMethod]
+        public void InitializeVariableTest()
+        {
+            String variableName = "Test";
+            int value = 50;
+            //Passes values as parameters to Test.MoveTo
+            Test.InitaliseVariable(variableName, value);
+            
+            //Checks shape is the same size as expected
+            Assert.IsTrue(variableName == Test.CurrentVariableName);
+            Assert.IsTrue(value == Test.CurrentVariableValue);
+        }
+        [TestMethod]
+        public void LoopTest()
+        {
+            int startLine = 10;
+            int endLine = 15;
+            //Passes values as parameters to Test.MoveTo
+            Test.Loop(startLine, endLine);
+
+            //Checks shape is the same size as expected
+            Assert.IsTrue(startLine == Test.StartLine);
+            Assert.IsTrue(endLine == Test.EndLine);
+        }
+        [TestMethod]
+        public void IfStatementTest()
+        {
+            int startLine = 10;
+            int endLine = 15;
+            //Passes values as parameters to Test.MoveTo
+            Test.IfStatement(startLine, endLine);
+
+            //Checks shape is the same size as expected
+            Assert.IsTrue(startLine == Test.StartLine);
+            Assert.IsTrue(endLine == Test.EndLine);
+        }
+        [TestMethod]
+        public void MethodsTest()
+        {
+            string methodName = "MyMethod";
+            string[] Parameters = new string [100];
+            Parameters.Append("188");
+            Parameters.Append("200");
+            Parameters.Append("300");
+            //Passes values as parameters to Test.MoveTo
+            Test.Methods(methodName, Parameters);
+
+            //Checks shape is the same size as expected
+            Assert.IsTrue(methodName.Equals(Test.CurrentMethodName));
+            Assert.AreEqual(Parameters[0], Test.CurrentParameters[0]);
+            Assert.AreEqual(Parameters[1], Test.CurrentParameters[1]);
+            Assert.AreEqual(Parameters[2], Test.CurrentParameters[2]);
         }
     }
 }
